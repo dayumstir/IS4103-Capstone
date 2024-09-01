dbae961659fe77097792fd71d1b9e5142e06ab69
Zhenyubbx <zychin000@gmail.com>
2024-09-02 02:30:58 +0800

    Added folder structure for mobile frontend

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,33 +1,31 @@
 import { StyleSheet, Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
-import { Button } from "@repo/ui";
+import { Button } from "@ant-design/react-native";
+import { store } from "./src/store";
+import { Provider } from "react-redux";
 
-export default function Native() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.header}>Native</Text>
-      <Button
-        onClick={() => {
-          console.log("Pressed!");
-          alert("Pressed!");
-        }}
-        text="Boop"
-      />
-      <StatusBar style="auto" />
-    </View>
-  );
+export default function App() {
+    return (
+        <Provider store={store}>
+            <View style={styles.container}>
+                <Text style={styles.header}>Native</Text>
+                <Button type="primary">primary</Button>
+                <StatusBar style="auto" />
+            </View>
+        </Provider>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  header: {
-    fontWeight: "bold",
-    marginBottom: 20,
-    fontSize: 36,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    header: {
+        fontWeight: "bold",
+        marginBottom: 20,
+        fontSize: 36,
+    },
 });
