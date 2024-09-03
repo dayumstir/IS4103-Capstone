2b2c347569a205c26fcc9d8295e3e2e602fd827f
dayumstir <dempstertay123@gmail.com>
2024-09-03 15:45:21 +0800

    Add tailwind + prettier to mobile

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,31 +1,19 @@
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Button } from "@ant-design/react-native";
 import { store } from "./src/store";
 import { Provider } from "react-redux";
+import "./globals.css";
 
 export default function App() {
-    return (
-        <Provider store={store}>
-            <View style={styles.container}>
-                <Text style={styles.header}>Native</Text>
-                <Button type="primary">primary</Button>
-                <StatusBar style="auto" />
-            </View>
-        </Provider>
-    );
+  return (
+    <Provider store={store}>
+      <View className="flex items-center justify-center bg-white">
+        <Text className="mb-5 text-4xl font-bold">Native</Text>
+        <Button type="primary">primary</Button>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
+  );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    header: {
-        fontWeight: "bold",
-        marginBottom: 20,
-        fontSize: 36,
-    },
-});
