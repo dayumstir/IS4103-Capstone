912f1f217e08aa980553d8e83efce0b2d5f59617
Jing Zhi <e0773051@u.nus.edu>
2024-09-08 01:55:35 +0800

    Remove error middleware

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,10 +1,8 @@
 // Entry point of application, where server is started
 // TODO: Add code to connect to the database when starting server.
 
-import { error } from "console";
 import app from "./app";
 import { connectDatabase } from "./config/database.config";
-import errorMiddleware from "./middlewares/errorMiddleware";
 
 const PORT = process.env.PORT || 3000;
 
@@ -17,5 +15,3 @@ connectDatabase()
     .catch((err => {
         console.error('Failed to connect to the database', err);
     }))
-
-app.use(errorMiddleware);
