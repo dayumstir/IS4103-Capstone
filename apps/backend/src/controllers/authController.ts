a8ea1c9b1685f6d5cfc42d08160d5fce95775c65
Jing Zhi <e0773051@u.nus.edu>
2024-09-08 00:43:27 +0800

    Add error handling

--- apps/backend/src/controllers/authController.ts
+++ apps/backend/src/controllers/authController.ts
@@ -3,7 +3,10 @@ import { Request, Response } from "express";
 import * as authService from "../services/authService";
 
 export const register = async (req: Request, res: Response) => {
-    // TODO: Add error handling and error codes
-    const customer = await authService.register(req.body);
-    res.status(201).json(customer);
+    try {
+        const customer = await authService.register(req.body);
+        res.status(201).json(customer);
+    } catch (error: any) {
+        res.status(400).json({ error: error.message});
+    }
 };
--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,10 +1,21 @@
 // Entry point of application, where server is started
 // TODO: Add code to connect to the database when starting server.
 
+import { error } from "console";
 import app from "./app";
+import { connectDatabase } from "./config/database.config";
+import errorMiddleware from "./middlewares/errorMiddleware";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+connectDatabase()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((err => {
+        console.error('Failed to connect to the database', err);
+    }))
+
+app.use(errorMiddleware);
