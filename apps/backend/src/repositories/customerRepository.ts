4e650e0147e7af1b58bf5c0ddc559afc5d8ce0e6
dayumstir <dempstertay123@gmail.com>
2024-09-07 20:48:48 +0800

    Refactor prisma + update customer interface

--- apps/backend/src/repositories/customerRepository.ts
+++ apps/backend/src/repositories/customerRepository.ts
@@ -1,15 +1,14 @@
 // Handles database operations related to customers
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "./db";
 import { ICustomer } from "../interfaces/customerInterface";
-
-const prisma = new PrismaClient();
 
 // Find customer by email (unique attribute) in db
 export const findCustomerByEmail = async (email: string) => {
-    return prisma.customer.findUnique({ where: { email } });
+  return prisma.customer.findUnique({ where: { email } });
 };
 
 // Create a new customer in db
 export const createCustomer = async (customerData: ICustomer) => {
-    return prisma.customer.create({ data: customerData });
-}
+  return prisma.customer.create({ data: customerData });
+};
+  
--- apps/backend/src/services/authService.ts
+++ apps/backend/src/services/authService.ts
@@ -1,31 +1,30 @@
 // Contains authentication logic, like JWT generation, verification
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import { Customer } from "../models/customerModel";
 import { ICustomer } from "../interfaces/customerInterface";
 import * as customerRepository from "../repositories/customerRepository";
 
-export const register = async(customerData: ICustomer) => {
-    const { email, password } = customerData;
+export const register = async (customerData: ICustomer) => {
+  const { email, password } = customerData;
 
-    // Check for existing customer in db
-    const existingCustomer = await customerRepository.findCustomerByEmail(email);
-    if (existingCustomer) {
-        throw new Error("Customer already exists");
-    }
+  // Check for existing customer in db
+  const existingCustomer = await customerRepository.findCustomerByEmail(email);
+  if (existingCustomer) {
+    throw new Error("Customer already exists");
+  }
 
-    // Hash password
-    const hashedPassword = await bcrypt.hash(password, 10);
+  // Hash password
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Create customer record in db
-    const newCustomer = await customerRepository.createCustomer({
-        ...customerData,
-        password: hashedPassword,
-    });
+  // Create customer record in db
+  const newCustomer = await customerRepository.createCustomer({
+    ...customerData,
+    password: hashedPassword,
+  });
 
-    // Generate JWT
-    // TODO: Create .env folder with JWT Secret
-    // const token = jwt.sign({ customerId: newCustomer.id }, process.env.JWT_SECRET!, { expiresIn: "1h"});
+  // Generate JWT
+  // TODO: Create .env folder with JWT Secret
+  // const token = jwt.sign({ customerId: newCustomer.id }, process.env.JWT_SECRET!, { expiresIn: "1h"});
 
-    return { customer: newCustomer };
-}
+  return { customer: newCustomer };
+};
