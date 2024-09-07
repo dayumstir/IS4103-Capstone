6a1c1f18a9ae63a3a5a844b0fb0e08b3dae323ec
Jing Zhi <e0773051@u.nus.edu>
2024-09-08 03:18:08 +0800

    Implement login endpoint

--- apps/backend/src/controllers/authController.ts
+++ apps/backend/src/controllers/authController.ts
@@ -2,11 +2,22 @@
 import { Request, Response } from "express";
 import * as authService from "../services/authService";
 
+
 export const register = async (req: Request, res: Response) => {
     try {
         const customer = await authService.register(req.body);
         res.status(201).json(customer);
     } catch (error: any) {
-        res.status(400).json({ error: error.message});
+        res.status(400).json({ error: error.message });
     }
 };
+
+
+export const login = async (req: Request, res: Response) => {
+    try {
+        const token = await authService.login(req.body);
+        res.status(200).json({ token });
+    } catch (error: any) {
+        res.status(400).json({ error: error.message });
+    }
+}
--- apps/backend/src/services/authService.ts
+++ apps/backend/src/services/authService.ts
@@ -4,6 +4,7 @@ import jwt from "jsonwebtoken";
 import { ICustomer } from "../interfaces/customerInterface";
 import * as customerRepository from "../repositories/customerRepository";
 
+
 export const register = async (customerData: ICustomer) => {
     const { email, password } = customerData;
 
@@ -24,7 +25,29 @@ export const register = async (customerData: ICustomer) => {
 
     // Generate JWT
     // TODO: Create .env folder with JWT Secret
-    // const token = jwt.sign({ customerId: newCustomer.customer_id }, process.env.JWT_SECRET!, { expiresIn: "1h"});
+    const token = jwt.sign({ customerId: newCustomer.customer_id }, process.env.JWT_SECRET!, { expiresIn: "1h"});
 
-    return { customer: newCustomer };
+    return { customer: newCustomer, token };
 };
+
+
+export const login = async (loginData: { email: string; password: string }) => {
+    const { email, password } = loginData;
+
+    // Check for existing customer in db
+    const customer = await customerRepository.findCustomerByEmail(email);
+    if (!customer) {
+        throw new Error("Invalid credentials");
+    }
+
+    // Check if passwords match
+    const isPasswordValid = await bcrypt.compare(password, customer.password);
+    if (!isPasswordValid) {
+        throw new Error("Invalid credentials");
+    }
+
+    // Generate JWT
+    const token = jwt.sign({ customerId: customer.customer_id }, process.env.JWT_SECRET!, { expiresIn: "1h"});
+
+    return token;
+}
