c3b1bbd3380563f5e8575ee6bac742cfe374dfff
Khoo Jing Zhi <88877544+JzKhoo@users.noreply.github.com>
2024-09-15 22:19:10 +0800

    Refactor customer register order (#10)
    * Implement phone number OTP
    
    * Define status enum, define relationships between Customer, EmailVerificationToken, Otp
    
    * Stricter check for valid email tokens
    
    * Allow login only for active accounts

--- apps/backend/src/routes/customerAuthRoutes.ts
+++ apps/backend/src/routes/customerAuthRoutes.ts
@@ -1,12 +1,14 @@
 // Defines routes related to authentication
 import { Router } from "express";
-import { sendConfirmationEmail, confirmEmailAndRegister, login, resetPassword, logout } from "../controllers/customerAuthController";
+import { registerCustomer, confirmEmail, sendPhoneNumberOTP, verifyPhoneNumberOTP, login, logout, resetPassword } from "../controllers/customerAuthController";
 import { customerAuthMiddleware } from "../middlewares/customerAuthMiddleware";
 
 const router = Router();
 
-router.post("/send-confirmation-email", sendConfirmationEmail);
-router.post("/confirm-email-and-register", confirmEmailAndRegister);
+router.post("/register", registerCustomer);
+router.post("/confirm-email", confirmEmail);
+router.post("/send-phone-number-otp", sendPhoneNumberOTP);
+router.post("/verify-phone-number-otp", verifyPhoneNumberOTP);
 router.post("/login", login);
 router.post("/logout", customerAuthMiddleware, logout);
 router.post("/reset-password", customerAuthMiddleware, resetPassword);
