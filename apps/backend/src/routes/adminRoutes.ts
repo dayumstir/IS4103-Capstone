f0220e9cbc43664efe5077976aa28801c12cd615
weekiatism <limweekiat99@gmail.com>
2024-09-21 21:27:00 +0800

    create admin by superadmin (#24)

--- apps/backend/src/routes/adminRoutes.ts
+++ apps/backend/src/routes/adminRoutes.ts
@@ -1,6 +1,7 @@
 // Defines routes related to admin actions
 import { Router } from "express";
 import { get, edit } from "../controllers/adminController";
+import { add } from "../controllers/adminAuthController";
 import { adminAuthMiddleware } from "../middlewares/adminAuthMiddleware";
 
 const router = Router();
@@ -8,5 +9,6 @@ const router = Router();
 router.get("/profile", adminAuthMiddleware, get);
 router.get("/editprofile", adminAuthMiddleware, get);
 router.put("/profile", adminAuthMiddleware, edit);
+router.post("/add", add);
 
 export default router;
