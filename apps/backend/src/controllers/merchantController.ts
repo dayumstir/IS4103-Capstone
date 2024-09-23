4a231f25c259d27533d7f6bacb3272ecec958df4
Zhenyubbx <79489486+Zhenyubbx@users.noreply.github.com>
2024-09-23 13:27:08 +0800

    Zy/fix/merchant edit profile (#44)
    * Completed merchant verify email and fix register merchant
    
    * fix merchant verify email
    
    * fix merchant edit profile

--- apps/backend/src/controllers/merchantController.ts
+++ apps/backend/src/controllers/merchantController.ts
@@ -5,40 +5,41 @@ import logger from "../utils/logger";
 
 // Merchant View Profile
 export const getMerchantProfile = async (req: Request, res: Response) => {
-  try {
-    const merchantId = req.params.merchant_id || req.body.merchant_id;
+    try {
+        const merchantId = req.params.merchant_id || req.body.merchant_id;
 
-    if (!merchantId) {
-      return res.status(400).json({ error: "Merchant ID is required" });
+        if (!merchantId) {
+            return res.status(400).json({ error: "Merchant ID is required" });
+        }
+        const merchant = await merchantService.getMerchantById(merchantId);
+        res.status(200).json(merchant);
+    } catch (error: any) {
+        res.status(400).json({ error: error.message });
     }
-    const merchant = await merchantService.getMerchantById(merchantId);
-    res.status(200).json(merchant);
-  } catch (error: any) {
-    res.status(400).json({ error: error.message });
-  }
 };
 
 // Merchant Edit Profile
 export const editMerchantProfile = async (req: Request, res: Response) => {
-  try {
-    const updatedMerchant = await merchantService.updateMerchant(
-      req.body.merchant_id,
-      req.body,
-      req.file?.buffer
-    );
-    res.status(200).json(updatedMerchant);
-  } catch (error: any) {
-    res.status(400).json({ error: error.message });
-  }
+    const id = req.params.id;
+    try {
+        const updatedMerchant = await merchantService.updateMerchant(
+            id,
+            req.body,
+            req.file?.buffer
+        );
+        res.status(200).json(updatedMerchant);
+    } catch (error: any) {
+        res.status(400).json({ error: error.message });
+    }
 };
 
 // List All Merchants
 export const listAllMerchants = async (req: Request, res: Response) => {
-  logger.info("Executing listAllMerchants...");
-  try {
-    const merchants = await merchantService.getAllMerchants();
-    res.status(200).json(merchants);
-  } catch (error: any) {
-    res.status(400).json({ error: error.message });
-  }
+    logger.info("Executing listAllMerchants...");
+    try {
+        const merchants = await merchantService.getAllMerchants();
+        res.status(200).json(merchants);
+    } catch (error: any) {
+        res.status(400).json({ error: error.message });
+    }
 };
