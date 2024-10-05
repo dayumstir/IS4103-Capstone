7e11f9313bc43d3d82b2c72651ccc6a24678c46c
Zhenyubbx <zychin000@gmail.com>
2024-10-05 17:07:51 +0800

    Refactor base query to auto logout merchant if token is invalid

--- apps/web-merchant/src/redux/services/issue.ts
+++ apps/web-merchant/src/redux/services/issue.ts
@@ -1,25 +1,17 @@
 // Need to use the React-specific entry point to import createApi
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi } from "@reduxjs/toolkit/query/react";
 
 import {
   IIssue,
   IssueFilter,
   IssueStatus,
 } from "../../interfaces/models/issueInterface";
+import BaseQueryWithAuthCheck from "../utils.tsx/baseQuery";
 
 // Define a service using a base URL and expected endpoints
 export const issueApi = createApi({
   reducerPath: "issueApi",
-  baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3000/issue",
-    prepareHeaders: (headers) => {
-      const token = localStorage.getItem("token");
-      if (token) {
-        headers.set("Authorization", `Bearer ${token}`);
-      }
-      return headers;
-    },
-  }),
+  baseQuery: BaseQueryWithAuthCheck("http://localhost:3000/issue"),
   endpoints: (builder) => ({
     createIssue: builder.mutation<IIssue, FormData>({
       query: (body) => ({
