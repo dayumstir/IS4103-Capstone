223e93ffc963fb4165a28f586706ae725032a17e
dayumstir <dempstertay123@gmail.com>
2024-10-24 21:13:06 +0800

    fix instalment payment details header

--- apps/mobile/app/(authenticated)/(tabs)/payments/instalments/_layout.tsx
+++ apps/mobile/app/(authenticated)/(tabs)/payments/instalments/_layout.tsx
@@ -1,5 +1,7 @@
 // payments/instalments/_layout.tsx
-import { Stack } from "expo-router";
+import { router, Stack } from "expo-router";
+import { Text, TouchableOpacity } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function InstalmentsLayout() {
   return (
@@ -8,7 +10,15 @@ export default function InstalmentsLayout() {
         name="[instalmentPaymentId]"
         options={{
           headerTitle: "Payment Details",
-          headerShown: false,
+          headerLeft: () => (
+            <TouchableOpacity
+              className="flex-row gap-1"
+              onPress={() => router.back()}
+            >
+              <Ionicons name="chevron-back" size={24} color="#3b82f6" />
+              <Text className="text-xl text-blue-500">Back</Text>
+            </TouchableOpacity>
+          ),
         }}
       />
     </Stack>
