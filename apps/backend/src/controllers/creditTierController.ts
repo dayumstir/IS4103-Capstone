f2d8107f32c4815f805705a7b1236d2c154c9be7
dayumstir <dempstertay123@gmail.com>
2024-11-09 23:12:51 +0800

    feat: delete credit tier

--- apps/backend/src/controllers/creditTierController.ts
+++ apps/backend/src/controllers/creditTierController.ts
@@ -46,3 +46,13 @@ export const editCreditTier = async (req: Request, res: Response) => {
         res.status(400).json({ error: error.message });
     }
 };
+
+// Delete Credit Tier
+export const deleteCreditTier = async (req: Request, res: Response) => {
+    try {
+        await creditTierService.deleteCreditTier(req.params.credit_tier_id);
+        res.status(200).json({ message: "Credit tier deleted successfully" });
+    } catch (error: any) {
+        res.status(400).json({ error: error.message });
+    }
+};
