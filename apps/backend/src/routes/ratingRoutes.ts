9b1eedbebc3f6cbfb959525e2aa578fce58f3966
Jing Zhi <e0773051@u.nus.edu>
2024-11-11 00:59:58 +0800

    Implement ratings backend

---
+++ apps/backend/src/routes/ratingRoutes.ts
@@ -0,0 +1,16 @@
+// app/backend/src/routes/ratingRoutes.ts
+import { Router } from "express";
+import { 
+    createRating, 
+    getRatings, 
+    getRating,
+} from "../controllers/ratingController";
+import { authMiddleware } from "../middlewares/authMiddleware";
+
+const router = Router();
+
+router.post("/add", authMiddleware, createRating);
+router.post("list", authMiddleware, getRatings);
+router.get("/:rating_id", authMiddleware, getRating);
+
+export default router;
