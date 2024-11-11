808412e6cf1f2199cef1b96a37720e17de37e1e2
angelalal <99884821+angelalal@users.noreply.github.com>
2024-11-11 21:08:32 +0800

    Admin send notifications (#135)
    * implemented auto notifs

--- apps/backend/src/repositories/issueRepository.ts
+++ apps/backend/src/repositories/issueRepository.ts
@@ -92,6 +92,9 @@ export const listAllIssuesWithSearch = async (search: string) => {
             merchant_id: true,
             customer_id: true,
             admin_id: true,
+            
+            transaction: true,
+            merchantPayment: true,
         },
     });
 };
